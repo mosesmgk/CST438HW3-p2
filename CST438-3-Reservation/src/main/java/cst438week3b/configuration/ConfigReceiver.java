fe609f38b2cc33baece2c2f945b4c8708ed8c6f6
mosesmgk <mlmamoses2@gmail.com>
2021-05-18 18:10:09 -0700

    Java files for PR

---
+++ CST438-3-Reservation/src/main/java/cst438week3b/configuration/ConfigReceiver.java
@@ -0,0 +1,30 @@
+package cst438week3b.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cst438week3b.listener.ReservationEventHandler;
+
+@Configuration
+public class ConfigReceiver {
+    @Bean
+    public FanoutExchange fanout() {
+        return new FanoutExchange("city-reservation");
+    }
+    @Bean
+    public Queue queue1() {
+        return new Queue("city-reservation-q1");
+    }
+    @Bean
+    public Binding binding1(FanoutExchange fanout, Queue queue1) {
+        return BindingBuilder.bind(queue1).to(fanout);
+    }
+    @Bean
+    public ReservationEventHandler receiver() {
+        return new ReservationEventHandler();
+    }
+}
---
+++ CST438-3-Reservation/src/main/java/cst438week3b/listener/ReservationEventHandler.java
@@ -0,0 +1,18 @@
+package cst438week3b.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+@RabbitListener(queues = "city-reservation-q1")
+public class ReservationEventHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(ReservationEventHandler.class);
+
+    @RabbitHandler
+    public void receive(String in) {
+        System.out.println("Backend Received:" + in);
+        log.info(" [ReservationEventHandler] Received '" + in + "'");
+    }
+}
